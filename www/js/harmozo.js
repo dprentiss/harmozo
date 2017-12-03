9143d00d44265477f0d15b815262caad49b928eb
David Prentiss <dprentiss@gmail.com>
2017-12-03 17:22:21 -0500

    init www and js

---
+++ www/js/harmozo.js
@@ -0,0 +1,22 @@
+var scale = {
+    intervals: [
+        2, 2, 1, 2, 2, 2, 1
+    ]
+};
+
+var majorScaleIntervals = [
+        2, 2, 1, 2, 2, 2, 1
+];
+
+var pitchClassNames = [
+    "c", ["cis, des], d, [dis, ees]
+    
+];
+
+var getScalePitchesByIntervals = function(tonicPitch, intervals) {
+    scalePitches = [tonicPitch];
+    for (var i = 0; i < intervals.length; i++) {
+    }
+};
+
+
